7e44f47433548162a0acefe6acbeecb7c118f376
unknown <henrr.eng@gmail.com>
2022-01-29 10:46:03 -0300

    README

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export GOOGLE_APPLICATION_CREDENTIALS=/home/development/shared/Projects/aip/outros/service-account-key.json
+export GOOGLE_APPLICATION_CREDENTIALS=$PWD/service-account-key.json
 export PATH=$PATH:$(go env GOPATH)/bin
 export GOPATH=$(go env GOPATH)
 
